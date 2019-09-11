78d3bcfd8222cf800ac86e6ef47552028318b87a
Hunter Davis <hunterdavis730@gmail.com>
2019-09-11 17:02:57 -0600

    added categroy and term search aesthetics

--- src/components/BookView/index.js
+++ src/components/BookView/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Books from "../Books"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import bookViewStyles from "./bookView.module.scss"
 
 class BookView extends Component {
   state = {
     books: [this.props.books],
-    category: "all",
+    category: "Top Ten",
     searchParameter: undefined,
+    showing: "Top Ten",
   }
 
   handleInput = event => {
@@ -27,7 +29,55 @@ class BookView extends Component {
     console.log(this.state.books)
     return (
       <div>
-        something will go here
+        <div className={bookViewStyles.header}>
+          <div className={bookViewStyles.select}>
+            <h3>Filter Books by Category</h3>
+            <select>
+              <option className={bookViewStyles.options} value="0">
+                Top Ten
+              </option>
+              <option className={bookViewStyles.options} value="1">
+                Age 0-3
+              </option>
+              <option className={bookViewStyles.options} value="2">
+                Age 3-5
+              </option>
+              <option className={bookViewStyles.options} value="3">
+                9-12 YA
+              </option>
+              <option className={bookViewStyles.options} value="4">
+                Storytales
+              </option>
+              <option className={bookViewStyles.options} value="5">
+                Librarians
+              </option>
+              <option className={bookViewStyles.options} value="6">
+                Storytime
+              </option>
+              <option className={bookViewStyles.options} value="7">
+                Newbury
+              </option>
+            </select>
+          </div>
+
+          <div className={bookViewStyles.searchContainer}>
+            <h3>Search by title or author</h3>
+            <input
+              className={bookViewStyles.searchBar}
+              type="text"
+              placeholder="Search.."
+            ></input>
+            <button className={bookViewStyles.searchBtn}>
+              <FontAwesomeIcon icon={faSearch} />{" "}
+            </button>
+          </div>
+        </div>
+        <div className={bookViewStyles.results}>
+          <h2>
+            Results for{" "}
+            <span style={{ fontWeight: "bold" }}>{this.state.showing}</span>
+          </h2>
+        </div>
         <ul className={bookViewStyles.visibleBooks}>
           {this.state.books[0].map(index => {
             return <Books book={index} />
